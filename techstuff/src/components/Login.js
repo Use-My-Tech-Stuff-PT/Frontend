6f84fac9ee2efcab89016a4adf1d0a00cc5fbc49
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-08-21 17:30:54 -0700

    added reactstrap to dependecies; added formState and error state; onchange handler and post request in the submit handler made:

--- techstuff/src/components/Login.js
+++ techstuff/src/components/Login.js
@@ -1,48 +1,78 @@
 import React, { useState } from 'react';
-// props???
-export default function Login () {
-    const [user, setUser] = useState([
-        {
-            userEmail: "",
-            userPassword: ""
-        }
-    ]);
+import { Button, Form, Label, Input, FormText } from 'reactstrap';
+import axios from 'axios';
+
+export default function Login (props) {
+    const [user, setUser] = useState([]);
+
+    const [formState, setFormState] = useState({
+        username: "",
+        password: ""
+    });
+
+    const [errors, setErrors] = useState({
+        username: "",
+        password: ""
+    });
 
     const submitLoginForm = (event) => {
         event.preventDefault();
         // Post req?/Server verification?/Lost...?
+        axios
+        .post("https://use-my-tech-stuff-api.herokuapp.com/api/users/register", formState)
+        .then(response => {
+            alert('Log in successful', response)
+        })
+        .catch("error logging in")
+    }
+
+    const inputChange = e => {
+        e.persist();
+        const newSignIn = {
+            ...formState,
+            [e.target.name]:
+            e.target.type === "checkbox" ? e.target.checked : e.target.value
+        };
+        setFormState(newSignIn)
     }
     return(
         <div>
-            <form className="form-signin">
+            <Form className="form-signin">
                 <img class="mb-1" src="https://raw.githubusercontent.com/Use-My-Tech-Stuff-PT/Marketing-Page/orlando-rivera-jr/img/Use%20My%20Tech%20Horizontal%20Logo_Teal%20Icon.png" alt="" width="auto" height="72" />
                 <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-                <label for="inputEmail" class="sr-only">Email: </label>
-                <input 
-                    type="email" 
-                    id="inputEmail" 
+                <Label for="inputusername" class="sr-only">Username: </Label>
+                <Input 
+                    type="username" 
+                    id="inputusername" 
+                    name="username"
                     class="form-control" 
-                    placeholder="Email address" 
+                    placeholder="username" 
                     required="" 
-                    autofocus="" />
+                    autofocus=""
+                    onChange={inputChange}
+                    value={formState.username} />
                 <br />
-                <label for="inputPassword" class="sr-only">Password</label>
-                <input 
+                <Label for="inputPassword" class="sr-only">Password</Label>
+                <Input 
                     type="password" 
                     id="inputPassword" 
+                    name="password"
                     class="form-control" 
                     placeholder="Password" 
-                    required=""/>
+                    required=""
+                    onChange={inputChange}
+                    value={formState.password}/>
                 <div class="checkbox mb-3">
-                    <label>
-                        <input 
+                    <Label>
+                        <Input 
                         type="checkbox" 
-                        value="remember-me"/> Remember me
-                    </label>
+                        value="remember-me"
+                        onChange={inputChange}/> Remember me
+                    </Label>
                 </div>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                <p class="mt-5 mb-3 text-muted">©2020</p>
-            </form>
+                <Button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</Button>
+                <FormText class="mt-5 mb-3 text-muted">©2020</FormText>
+            </Form>
         </div>
     )
 };
