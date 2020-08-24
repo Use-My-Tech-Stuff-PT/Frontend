ba4302ddfe4345363103802d390433e7e6c3c4df
sirbaconsalot <donavynhaley@gmail.com>
2020-08-23 22:01:41 -0700

    Added Yup validation to form

--- techstuff/src/components/SignUp.js
+++ techstuff/src/components/SignUp.js
@@ -1,15 +1,55 @@
-import React, { useState } from "react";
-import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  FormFeedback,
+} from "reactstrap";
 import axios from "axios";
+import * as Yup from "yup";
 
 export default function SignUp() {
+  // Use State to create new users
   const [newUser, setNewUser] = useState({
     username: "",
     email: "",
     password: "",
     selection: "",
   });
+
+  // Error logging
+  const [errors, setErrors] = useState({
+    username: "",
+    email: "",
+    password: "",
+    selection: "",
+  });
+
+  // Use state to hold post data
   const [post, setPost] = useState([]);
+
+  // Use state to disable button if form is not valid
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  // checks if form is valid and catches errors and updates the errors state
+  const validateChange = (event) => {
+    Yup.reach(formSchema, event.target.name)
+      .validate(event.target.value)
+      .then((valid) => {
+        setErrors({
+          ...errors,
+          [event.target.name]: "",
+        });
+      })
+      .catch((err) => {
+        setErrors({ ...errors, [event.target.name]: err.errors[0] });
+      });
+  };
+
+  // Makes post request and users data into post state then clears setnew user for testing
   const handleSubmit = (event) => {
     event.preventDefault();
     axios.post("https://reqres.in/api/users", newUser).then((res) => {
@@ -23,12 +63,36 @@ export default function SignUp() {
       });
     });
   };
+
+  // Handles changes in form and updates newUser state
   const handleChange = (event) => {
+    event.persist();
+    validateChange(event);
     setNewUser({
       ...newUser,
       [event.target.name]: event.target.value,
     });
   };
+
+  // Shaping object data with correct requirements for validation
+  const formSchema = Yup.object().shape({
+    username: Yup.string().required("Must include a username"),
+    email: Yup.string()
+      .email("Must be a valid email address.")
+      .required("Must include email address."),
+    password: Yup.string()
+      .min(6, "Passwords must be at least 6 characters long.")
+      .required("Password is Required"),
+    selection: Yup.string().required("Please make your selection"),
+  });
+
+  // Updates the button to enabled if the form is valid
+  useEffect(() => {
+    formSchema.isValid(newUser).then((valid) => {
+      setButtonDisabled(!valid);
+    });
+  }, [newUser]);
+
   return (
     <div id="formContainer">
       <h2>Sign Up</h2>
@@ -43,6 +107,9 @@ export default function SignUp() {
             value={newUser.username}
             onChange={(event) => handleChange(event)}
           />
+          {errors.username.length > 0 ? (
+            <p className="error">{errors.username}</p>
+          ) : null}
         </FormGroup>
         <FormGroup>
           <Label for="userEmail">Email</Label>
@@ -54,6 +121,9 @@ export default function SignUp() {
             value={newUser.email}
             onChange={(event) => handleChange(event)}
           />
+          {errors.email.length > 0 ? (
+            <p className="error">{errors.email}</p>
+          ) : null}
         </FormGroup>
         <FormGroup>
           <Label for="userPassword">Password</Label>
@@ -65,6 +135,9 @@ export default function SignUp() {
             value={newUser.password}
             onChange={(event) => handleChange(event)}
           />
+          {errors.password.length > 0 ? (
+            <p className="error">{errors.password}</p>
+          ) : null}
         </FormGroup>
         <FormGroup>
           <Label for="idSelection">Renter or Owner</Label>
@@ -76,12 +149,17 @@ export default function SignUp() {
             value={newUser.selection}
             onChange={(event) => handleChange(event)}
           >
-            <option></option>
+            <option disabled></option>
             <option value="Renter">Renter</option>
             <option value="Owner">Owner</option>
           </Input>
+          {errors.selection.length > 0 ? (
+            <p className="error">{errors.selection}</p>
+          ) : null}
         </FormGroup>
-        <Button color="primary">Submit</Button>
+        <Button color="primary" disabled={buttonDisabled}>
+          Submit
+        </Button>
       </Form>
     </div>
   );
